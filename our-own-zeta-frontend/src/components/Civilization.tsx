223e00b6de03b2fc8ca9a7fb8c0ace1c68662ea1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-27 13:52:40 +0100

    Civilization component basic structure

--- our-own-zeta-frontend/src/components/Civilization.tsx
+++ our-own-zeta-frontend/src/components/Civilization.tsx
@@ -1,7 +1,26 @@
 function Civilization() {
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Name</h1>
+      <h2>Age: X</h2>
+      <h3>Population: X</h3>
+      <ul>
+        Resources:
+        <li>Example 1</li>
+        <li>Example 2</li>
+      </ul>
+      <ul>
+        Discoveries:
+        <li>Example 1</li>
+        <li>Example 2</li>
+      </ul>
+      <ul>
+        Important Individuals:
+        <li>Individual 1, Knowledge: Discoveries: </li>
+        <li>Individual 2, Knowledge: Discoveries:</li>
+      </ul>
+      <p>History Log:</p>
+      <textarea>Event 1 Event 2</textarea>
     </div>
   );
 }
