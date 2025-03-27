268c9ac575f52ed51528c00ff1a60faff5b09b98
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-27 15:44:21 +0100

    Adding important person example

--- our-own-zeta-frontend/src/components/ImportantPerson.tsx
+++ our-own-zeta-frontend/src/components/ImportantPerson.tsx
@@ -1,5 +1,36 @@
+/*
+    "Buel": {
+        "age": 0,
+        "name": "Buel",
+        "field": "Athlete",
+        "discoveries": {},
+        "alive": true,
+        "knowledge": 0,
+        "lifeLog": [
+            "Buel, a promising athlete was born!"
+        ]
+*/
+
 function ImportantPerson() {
-  return <div>Hi, I am an important person!</div>;
+  return (
+    <div>
+      <h2>Name: Pepe</h2>
+      <h3>Scientist</h3>
+      <p>Age: 36</p>
+      <p>Knowledge: 94</p>
+      <p>Discoveries:</p>
+      <ul>
+        <li>Discovery 1</li>
+        <li>Discovery 2</li>
+      </ul>
+      <p>Life story:</p>
+      <ul>
+        <li>Pepe was born</li>
+        <li>Pepe discovered Electricity 1</li>
+        <li>Pepe started studying programming at {"</salt>"}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default ImportantPerson;
