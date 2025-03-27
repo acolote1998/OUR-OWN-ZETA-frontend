aafcb5e0e704493e73cebcbde7ead09d727cc423
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-27 15:29:24 +0100

    Civ: adding props Test object

--- our-own-zeta-frontend/src/App.tsx
+++ our-own-zeta-frontend/src/App.tsx
@@ -2,9 +2,88 @@ import "./App.css";
 import Civilization from "./components/Civilization";
 
 function App() {
+  /*
+  interface Person {
+  age: number;
+  name: string;
+  field: string;
+  discoveries: Map<string, number>;
+  alive: boolean;
+  knowledge: number;
+  lifelog: string[];
+}
+
+type CivilizationProps = {
+  historyLog: string[];
+};
+  */
+  const civProprsTest = {
+    name: "The Testians",
+    age: 50,
+    population: 500,
+    resources: new Map([
+      ["Gold", 1],
+      ["Technology", 2],
+      ["Diamonds", 1],
+    ]),
+    discoveries: new Map([
+      ["Electricity", 1],
+      ["Fire", 4],
+      ["Physics", 2],
+    ]),
+    importantIndividuals: new Map([
+      [
+        "Aki",
+        {
+          age: 27,
+          name: "Aki",
+          field: "Scientist",
+          discoveries: new Map([
+            ["Programming", 2],
+            ["Gaming", 1],
+          ]),
+          alive: true,
+          knowledge: 98,
+          lifelog: [
+            "Aki moved to Sweden",
+            "Aki got a job at Disney",
+            "Aki left his job at Disney",
+            "Aki got a job at Nike",
+            "Aki got a permanent residency in Sweden",
+            "Aki left his job at Nike",
+            "Aki got a place in the <salt> program",
+            "Aki is happy",
+          ],
+        },
+      ],
+      [
+        "Michaela",
+        {
+          age: 27,
+          name: "Michaela",
+          field: "Hard Worker",
+          discoveries: new Map([
+            ["Happines", 4],
+            ["Loving", 5],
+          ]),
+          alive: true,
+          knowledge: 98,
+          lifelog: [
+            "Michaela met Aki",
+            "Michaela and Aki are very happy together!",
+          ],
+        },
+      ],
+    ]),
+    historyLog: [
+      "Civilization was created",
+      "Aki joined the Civilization",
+      "Michaela joined the Civlization",
+    ],
+  };
   return (
     <>
-      <Civilization />
+      <Civilization {...civProprsTest} />
     </>
   );
 }
